befc17649527215716df1904bb6bb33d87018a39
Korrigon <korrig@goletan.tech>
2024-11-17 15:58:05 +0100

    - fix config

--- pkg/graphql.go
+++ pkg/graphql.go
@@ -4,17 +4,11 @@ package graphql
 import (
 	observability "github.com/goletan/observability/pkg"
 	services "github.com/goletan/services/pkg"
-	"go.uber.org/zap"
 
 	"github.com/goletan/api-graphql/internal/server"
 )
 
 // NewGraphQLService creates a new GraphQL service that implements the Goletan service interface.
-func NewGraphQLService(logger *zap.Logger) services.Service {
-	obs, err := observability.NewObserver()
-	if err != nil {
-		logger.Error("Failed to initialize observability", zap.Error(err))
-	}
-
+func NewGraphQLService(obs *observability.Observability) services.Service {
 	return server.NewGraphQLServer(obs)
 }
